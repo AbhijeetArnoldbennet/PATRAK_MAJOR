d9f6e940776476cd333375493a5725c0e7eca2d4
Abhijeet <abhijeetab05@gmail.com>
2026-08-03 12:38:57 +0530

    Initial project setup with React and Home page

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to PATRAK</h1>
+      <p>Cloud-Based Multi-Tenant Clinic Management Platform</p>
+    </div>
+  );
+}
+
+export default Home;
